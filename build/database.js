91f3e5b37b7d7348069c71164cd814e3d4ec1909
carolross1 <115049387+carolross1@users.noreply.github.com>
2024-11-29 22:01:16 -0600

    mysql2

--- build/database.js
+++ build/database.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const promise_mysql_1 = __importDefault(require("promise-mysql"));
+const promise_mysql_1 = __importDefault(require("promise-mysql2"));
 const keys_1 = __importDefault(require("./keys"));
 const pool = promise_mysql_1.default.createPool(keys_1.default.database);
 pool.getConnection().then(connection => {
